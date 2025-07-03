098b8f25fe1afd2fc86b836c5f39cef827a6b590
Teja chilumula <66719648+TejaChilumula@users.noreply.github.com>
2025-07-02 23:49:11 -0700

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 7/2/2025, 11:49:11 PM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
