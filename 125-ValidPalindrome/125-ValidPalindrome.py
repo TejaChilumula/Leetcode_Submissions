a8352542828010fe04dd79123d5ffe8ff61ecb5e
Teja chilumula <66719648+TejaChilumula@users.noreply.github.com>
2025-07-02 23:53:55 -0700

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,21 @@
+# Last updated: 7/2/2025, 11:53:55 PM
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        i = 0
+        j = len(s)-1
+        
+        while i<=j:
+            
+            if  not s[i].isalnum():
+                i+=1
+            elif not s[j].isalnum():
+                j-=1
+            elif s[i] != s[j]:
+                return False
+            else:
+                i += 1
+                j -= 1
+        
+        return True
+        
