c9e5e4a308d6bdee7a19b90cbec634f118c90efd
Teja chilumula <66719648+TejaChilumula@users.noreply.github.com>
2025-07-02 23:49:58 -0700

    Create 2058-ConcatenationOfArray

---
+++ 2058-ConcatenationOfArray/2058-ConcatenationOfArray.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/2/2025, 11:49:58 PM
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<2*nums.size();i++)
+        {
+            if(i<nums.size())
+            ans.push_back(nums[i]);
+            else
+                ans.push_back(nums[i-nums.size()]);
+        }
+        return ans;
+    }
+}; 
