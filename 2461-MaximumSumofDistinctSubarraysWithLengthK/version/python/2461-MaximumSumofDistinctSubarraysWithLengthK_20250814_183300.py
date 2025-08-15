79f24d2a130101648324171edf977895112f7ab2
Teja chilumula <66719648+TejaChilumula@users.noreply.github.com>
2025-08-14 18:33:00 -0700

    Create 2461-MaximumSumofDistinctSubarraysWithLengthK

---
+++ 2461-MaximumSumofDistinctSubarraysWithLengthK/version/python/2461-MaximumSumofDistinctSubarraysWithLengthK_20250814_183300.py
@@ -0,0 +1,31 @@
+# Last updated: 8/14/2025, 6:33:00 PM
+# Sliding window , generic with python defaultdict
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+
+        freq = defaultdict(int)
+        counter, wsum, maxi = 0, 0, 0
+        start, n = 0, len(nums)
+
+        for end in range(n):
+            freq[nums[end]] += 1
+            counter += 1
+            wsum += nums[end]
+
+            if counter > k:
+                counter -= 1
+                freq[nums[start]] -= 1
+                wsum -= nums[start]
+
+                if freq[nums[start]] == 0:
+                    del freq[nums[start]]
+                
+                start += 1
+            
+            if len(freq) == k:
+                maxi = max(maxi, wsum)
+
+        return maxi
+
+
+        
